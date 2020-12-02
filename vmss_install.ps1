398140beb3002ceed6036cc8d236a622f2f4f3be
Naitik <Naitik@bhoganikhilgmail.onmicrosoft.com>
2020-12-02 16:42:14 +0530

    new code

---
+++ vmss_install.ps1
@@ -0,0 +1,35 @@
+﻿
+#installing local accounts on vm
+
+[string[]]$useraccounts = 'dbadmin','backupadmin','devaccount'
+$passwords = ConvertTo-SecureString '$y$@dmin_2020' -AsPlainText -Force 
+foreach ($users in $useraccounts)
+{
+    New-LocalUser -Name $users -AccountNeverExpires -Description "administrator accounts created using powershell" -FullName "this account belongs to $users" -Password $passwords 
+}
+foreach ($users in $useraccounts)
+{
+Add-LocalGroupMember -Group administrators -Member $useraccounts 
+}
+#installing iis on vm
+
+Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools 
+
+
+#removing default application, defaultsite and creating new application pool and new website
+[string]$oldwebappool= 'DefaultAppPool'
+Remove-WebAppPool -Name $oldwebappool
+
+Remove-Website -Name 'Default Web site'
+
+$newAppPool = New-WebAppPool -Name "mysite"
+
+New-WebSite -Name "mysite" -Port 80  -PhysicalPath "$env:systemdrive\inetpub\wwwroot" -ApplicationPool $newAppPool.name
+
+#installing firefox using on vmss
+
+$SourceURL = "https://redirector.gvt1.com/edgedl/release2/chrome/VM7AZGvxTD3eRDzvxYLSvg_86.0.4240.75/86.0.4240.75_chrome_installer.exe"
+$Installer = $env:TEMP + "\chrome.exe"
+Invoke-WebRequest $SourceURL -OutFile $Installer
+Start-Process -FilePath $Installer -Args "/s" -Verb RunAs -Wait
+Remove-Item -Path $Installer -Recurse -Force
